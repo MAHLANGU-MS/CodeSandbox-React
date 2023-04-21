91c942c99a62142b081f1b1e45fc578c6e92b10d
MAHLANGU-MS <108589923+MAHLANGU-MS@users.noreply.github.com>
2023-04-21 12:21:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,92 @@
+import React, { useState } from "react";
 import "./styles.css";
+import Login from "./components/Login";
 
-export default function App() {
+function App() {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm_password, setConfirmPassword] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [mouseOver, setMousedOver] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPasswordMatch(true);
+
+    if (!name) {
+      alert("Invalid Name");
+    } else if (!surname) {
+      alert("Invalid Surname");
+    } else if (!email || !email.includes("@") || !email.includes(".")) {
+      alert("Invalid Email");
+    } else if (!password) {
+      alert("Invalid Password");
+    } else if (password !== confirm_password) {
+      setPasswordMatch(false);
+      setConfirmPassword("");
+    } else {
+      alert("Successfully registered!");
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <p className="RegInfo">Enter Your Details To Register</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Surname"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirm_password}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {!passwordMatch && <p>Passwords do not match.</p>}
+
+        <button
+          type="submit"
+          style={{ backgroundColor: mouseOver ? "black" : "white" }}
+          onMouseOver={() => {
+            setMousedOver(true);
+          }}
+          onMouseOut={() => {
+            setMousedOver(false);
+          }}
+        >
+          Register{" "}
+        </button>
+      </form>
     </div>
+    // <Login />;
   );
 }
+
+export default App;
