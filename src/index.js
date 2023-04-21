7a8604904f50774325307e3a9fbe8ebb26809881
MAHLANGU-MS <108589923+MAHLANGU-MS@users.noreply.github.com>
2023-04-21 12:23:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
+
+//HINT: You'll need to apply the following things you learnt:
+//1. Using JS Objects with state.
+//2. Making use of previous state when changing state.
+//3. Working with forms in React.
+//4. Handing events
